94a373d15f1ef109a48917590a750ca8e16f41aa
jang-yujin <jyj9747@naver.com>
2023-05-27 12:16:36 +0900

    :sparkles: writing form validate & submit
    디비 연결 아직 안 한 상태

--- lib/write_order.dart
+++ lib/write_order.dart
@@ -1,13 +1,17 @@
+import 'dart:developer';
+import 'package:logger/logger.dart';
 import 'package:flutter/material.dart';
 import 'size.dart';
 
 final _formKey = GlobalKey<FormState>();
+const List<String> category_menu = <String>['category1', 'category2', 'category3', 'category4'];
 String _store_name = "";
 String _pickup_spot = "";
 int _member_count = 0;
 String _order_link = "";
-String _category = "";
+String _category = category_menu.first;
 late DateTime _dateTime;
+var logger = Logger();
 
 class WritePage extends StatefulWidget {
   const WritePage({super.key});
@@ -17,6 +21,7 @@ class WritePage extends StatefulWidget {
 }
 
 class _WritePageState extends State<WritePage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +47,10 @@ class _WritePageState extends State<WritePage> {
       body: Container(
         decoration: BoxDecoration(color: Color(0xFF98A5B3)),
         child: Column(
-          children: [WritingForm(), SizedBox()],
+          children: [
+            WritingForm(),
+            SizedBox()
+          ],
         ),
       ),
       bottomNavigationBar: Container(
@@ -54,15 +62,39 @@ class _WritePageState extends State<WritePage> {
             style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.resolveWith(
                     (color) => Color(0xFF284463))),
-            onPressed: () {},
+            onPressed: () {
+              final form = _formKey.currentState;
+              if(form != null && form.validate()){
+                form.save();
+                printFormValues();
+              }
+            },
           ),
         ),
       ),
     );
   }
+
+  void printFormValues() {
+    logger.i('가게 이름: $_store_name');
+    logger.i('받을 장소: $_pickup_spot');
+    logger.i('사람 수: $_member_count');
+    logger.i('배달의 민족 링크: $_order_link');
+    logger.i('카테고리: $_category');
+  }
 }
 
-Widget WritingForm() => Form(
+class WritingForm extends StatefulWidget {
+  const WritingForm({super.key});
+
+  @override
+  State<WritingForm> createState() => _WritingForm();
+}
+
+class _WritingForm extends State<WritingForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
       key: _formKey,
       child: Expanded(
         child: SingleChildScrollView(
@@ -73,13 +105,29 @@ Widget WritingForm() => Form(
               InputField("받을 장소: ", 1),
               InputField("사람 수: ", 2),
               InputField("배달의 민족 함께주문 링크: ", 3),
-              InputField("카테고리", 4),
+              DropdownButton(
+                value: _category,
+                icon: const Icon(Icons.arrow_downward),
+                onChanged: (String? value) {
+                  setState(() {
+                    _category = value!;
+                  });
+                },
+                items: category_menu.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              )
+              // InputField("카테고리", 4),
               // SizedBox(height: 16,),
             ],
           ),
         ),
       ),
     );
+  }
 
 Widget InputField(String text, int index) {
   return TextFormField(
@@ -90,25 +138,25 @@ Widget InputField(String text, int index) {
       else if (index == 1)
         _pickup_spot = value as String;
       else if (index == 2)
-        _member_count = value as int;
+        _member_count = int.parse(value!);
       else if (index == 3)
         _order_link = value as String;
-      else
-        _category = value as String;
     },
     validator: (value) {
       if (value == null || value.isEmpty) {
         if (index == 0) {
-          return '수신인 메일을 입력하세요';
+          return '가게 이름을 입력하세요';
+        } else if (index == 1) {
+          return '받을 장소를 입력하세요';
         } else if (index == 2) {
-          return '제목을 입력하세요';
+          return '사람 수를 입력하세요';
         } else if (index == 3) {
-          return '내용을 입력하세요';
+          return '배달의 민족 함께주문 링크를 입력하세요';
         }
       }
       return null;
     },
-    readOnly: (index == 1) ? true : false,
+    // readOnly: (index == 1) ? true : false,
     // initialValue: (index == 1)? FirebaseAuth.instance.currentUser!.email : null,
     keyboardType: (index == 3) ? TextInputType.multiline : TextInputType.text,
     // minLines: (index == 3) ? 40 : null,
@@ -125,4 +173,5 @@ Widget InputField(String text, int index) {
       //prefix: Text(text),
     ),
   );
+}
 }
